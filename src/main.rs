6c4fc0e319f3c21d1de9671a51b8fef64675cc96
Alex Studer <alex@studer.dev>
2020-10-14 00:22:15 -0400

    can now look up the provided category and print out all facts associated with that category

--- src/main.rs
+++ src/main.rs
@@ -3,11 +3,17 @@ use std::error::Error;
 use std::path::PathBuf;
 use structopt::StructOpt;
 use sqlite;
+use sqlite::Value;
 
 #[derive(Debug, StructOpt)]
 struct Opt {
+	#[structopt(short, long)]
 	database: PathBuf,
+
+	#[structopt(short, long)]
 	category: Option<String>
+	// from: String,
+	// to: String
 }
 
 fn main() -> Result<(), Box<dyn Error>> {
@@ -19,15 +25,36 @@ fn main() -> Result<(), Box<dyn Error>> {
 
 	let connection = sqlite::open(opt.database)?;
 
-	if let None = opt.category {
-		println!("You must specify a category!");
-		println!("Valid categories:");
-		connection.iterate("SELECT name FROM categories", |row| {
-			println!("* {}", row[0].1.unwrap());
-			true
-		})?;
+	let category_name = match opt.category {
+		Some(a) => a,
+		None => {
+			println!("You must specify a category with the --category flag!");
+			println!("Valid categories:");
+			connection.iterate("SELECT name FROM categories", |row| {
+				println!("* {}", row[0].1.unwrap());
+				true
+			})?;
 
-		return Ok(());
+			std::process::exit(0);
+		}
+	};
+
+	// find the ID of the specified category
+	let mut category_id_cursor = connection.prepare("SELECT id FROM categories WHERE name = ?").unwrap().cursor();
+	category_id_cursor.bind(&[Value::String(category_name)])?;
+	let row = category_id_cursor.next()?;
+	let category_id = row.unwrap()[0].as_integer().unwrap();
+
+	let mut facts_cursor = connection.prepare(
+		"
+		SELECT activities.name, facts.description, facts.start_time, facts.end_time FROM facts
+		INNER JOIN activities ON activities.id = facts.activity_id
+		WHERE activities.category_id = ?
+		"
+	).unwrap().cursor();
+	facts_cursor.bind(&[Value::Integer(category_id)])?;
+	while let Some(row) = facts_cursor.next().unwrap() {
+		println!("{:#?}", row);
 	}
 
 	Ok(())
